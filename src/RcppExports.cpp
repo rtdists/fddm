0b7a7924e6a2aa0ecdc6e2b036ce85c9a07b2fbe
kendalfoster <43751054+kendalfoster@users.noreply.github.com>
2020-02-12 23:07:05 +0000

    Something went wrong with the last commit & push

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,23 +6,23 @@
 using namespace Rcpp;
 
 // cpp_dfddm
-NumericVector cpp_dfddm(const NumericVector& rt, const LogicalVector& response, const double& a, const double& v, const double& t0, const double& w, const double& sv, const bool& log_prob, const std::string& n_terms_small, const std::string& summation_small, const std::string& scale, const double& eps);
+NumericVector cpp_dfddm(const NumericVector& rt, const LogicalVector& response, const NumericVector& a, const NumericVector& v, const NumericVector& t0, const NumericVector& w, const NumericVector& sv, const LogicalVector& log_prob, const std::string& n_terms_small, const std::string& summation_small, const std::string& scale, const NumericVector& eps);
 RcppExport SEXP _fddm_cpp_dfddm(SEXP rtSEXP, SEXP responseSEXP, SEXP aSEXP, SEXP vSEXP, SEXP t0SEXP, SEXP wSEXP, SEXP svSEXP, SEXP log_probSEXP, SEXP n_terms_smallSEXP, SEXP summation_smallSEXP, SEXP scaleSEXP, SEXP epsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< const NumericVector& >::type rt(rtSEXP);
     Rcpp::traits::input_parameter< const LogicalVector& >::type response(responseSEXP);
-    Rcpp::traits::input_parameter< const double& >::type a(aSEXP);
-    Rcpp::traits::input_parameter< const double& >::type v(vSEXP);
-    Rcpp::traits::input_parameter< const double& >::type t0(t0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type w(wSEXP);
-    Rcpp::traits::input_parameter< const double& >::type sv(svSEXP);
-    Rcpp::traits::input_parameter< const bool& >::type log_prob(log_probSEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type a(aSEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type v(vSEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type t0(t0SEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type w(wSEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type sv(svSEXP);
+    Rcpp::traits::input_parameter< const LogicalVector& >::type log_prob(log_probSEXP);
     Rcpp::traits::input_parameter< const std::string& >::type n_terms_small(n_terms_smallSEXP);
     Rcpp::traits::input_parameter< const std::string& >::type summation_small(summation_smallSEXP);
     Rcpp::traits::input_parameter< const std::string& >::type scale(scaleSEXP);
-    Rcpp::traits::input_parameter< const double& >::type eps(epsSEXP);
+    Rcpp::traits::input_parameter< const NumericVector& >::type eps(epsSEXP);
     rcpp_result_gen = Rcpp::wrap(cpp_dfddm(rt, response, a, v, t0, w, sv, log_prob, n_terms_small, summation_small, scale, eps));
     return rcpp_result_gen;
 END_RCPP
--- src/cpp_dfddm.cpp
+++ src/cpp_dfddm.cpp
@@ -12,127 +12,151 @@ using Rcpp::Rcerr;
 // [[Rcpp::export]]
 NumericVector cpp_dfddm(const NumericVector& rt,
                         const LogicalVector& response,
-                        const double& a, const double& v,
-                        const double& t0, const double& w,
-                        const double& sv, const bool& log_prob,
+                        const NumericVector& a, const NumericVector& v,
+                        const NumericVector& t0, const NumericVector& w,
+                        const NumericVector& sv, const LogicalVector& log_prob,
                         const std::string& n_terms_small,
                         const std::string& summation_small,
                         const std::string& scale,
-                        const double& eps)
+                        const NumericVector& eps)
 {
-  if (!log_prob) { // non-log version
-    if (scale == "small") {
-      if (summation_small == "2017") {
-        if (n_terms_small == "Foster") {
-          return fs_eps_2017(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Navarro") {
-          return fs_Nav_2017(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fs_BGK_2017(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else if (summation_small == "2014") {
-        if (n_terms_small == "Foster") {
-          return fs_eps_2014(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Navarro") {
-          return fs_Nav_2014(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fs_BGK_2014(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else {
-        Rcerr << "error: invalid summation_small" << endl;
-        return NAN;
-      }
-    } else if (scale == "large") {
-      return fl_Nav_2009(rt, response, a, v, t0, w, eps);
-    } else if (scale == "both") {
-      if (summation_small == "2017") {
-        if (n_terms_small == "Navarro") {
-          return fb_Nav_Nav_2017(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fb_BGK_Nav_2017(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else if (summation_small == "2014") {
-        if (n_terms_small == "Navarro") {
-          return fb_Nav_Nav_2014(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fb_BGK_Nav_2014(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else {
-        Rcerr << "error: invalid summation_small" << endl;
-        return NAN;
-      }
-    } else {
-      Rcerr << "error: invalid scale" << endl;
-      return NAN;
-    }
-  } else { // log version
-    if (scale == "small") {
-      if (summation_small == "2017") {
-        if (n_terms_small == "Foster") {
-          return fs_eps_2017_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Navarro") {
-          return fs_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fs_BGK_2017_log(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else if (summation_small == "2014") {
-        if (n_terms_small == "Foster") {
-          return fs_eps_2014_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Navarro") {
-          return fs_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fs_BGK_2014_log(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else {
-        Rcerr << "error: invalid summation_small" << endl;
-        return NAN;
-      }
-    } else if (scale == "large") {
-      return fl_Nav_2009_log(rt, response, a, v, t0, w, eps);
-    } else if (scale == "both") {
-      if (summation_small == "2017") {
-        if (n_terms_small == "Navarro") {
-          return fb_Nav_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fb_BGK_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else if (summation_small == "2014") {
-        if (n_terms_small == "Navarro") {
-          return fb_Nav_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
-        } else if (n_terms_small == "Kesselmeier") {
-          return fb_BGK_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
-        } else {
-          Rcerr << "error: invalid n_terms_small" << endl;
-          return NAN;
-        }
-      } else {
-        Rcerr << "error: invalid summation_small" << endl;
-        return NAN;
-      }
-    } else {
-      Rcerr << "error: invalid scale" << endl;
-      return NAN;
-    }
-  }
+  // if (min(rt.length(),
+  //         response.length(),
+  //         a.length(),
+  //         v.length(),
+  //         t0.length(),
+  //         w.length(),
+  //         sv.length(),
+  //         log_prob.length(),
+  //         eps.length()) < 1) {
+  //   Rcerr << "error: one of the inputs does not exist" << endl;
+  //   return NAN;
+  // }
+  
+  int Nmax = max({rt.length(),
+                 response.length(),
+                 a.length(),
+                 v.length(),
+                 t0.length(),
+                 w.length(),
+                 sv.length(),
+                 log_prob.length(),
+                 eps.length()});
+  return Nmax;
+  
+  // if (!log_prob) { // non-log version
+  //   if (scale == "small") {
+  //     if (summation_small == "2017") {
+  //       if (n_terms_small == "Foster") {
+  //         return fs_eps_2017(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Navarro") {
+  //         return fs_Nav_2017(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fs_BGK_2017(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else if (summation_small == "2014") {
+  //       if (n_terms_small == "Foster") {
+  //         return fs_eps_2014(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Navarro") {
+  //         return fs_Nav_2014(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fs_BGK_2014(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else {
+  //       Rcerr << "error: invalid summation_small" << endl;
+  //       return NAN;
+  //     }
+  //   } else if (scale == "large") {
+  //     return fl_Nav_2009(rt, response, a, v, t0, w, eps);
+  //   } else if (scale == "both") {
+  //     if (summation_small == "2017") {
+  //       if (n_terms_small == "Navarro") {
+  //         return fb_Nav_Nav_2017(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fb_BGK_Nav_2017(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else if (summation_small == "2014") {
+  //       if (n_terms_small == "Navarro") {
+  //         return fb_Nav_Nav_2014(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fb_BGK_Nav_2014(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else {
+  //       Rcerr << "error: invalid summation_small" << endl;
+  //       return NAN;
+  //     }
+  //   } else {
+  //     Rcerr << "error: invalid scale" << endl;
+  //     return NAN;
+  //   }
+  // } else { // log version
+  //   if (scale == "small") {
+  //     if (summation_small == "2017") {
+  //       if (n_terms_small == "Foster") {
+  //         return fs_eps_2017_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Navarro") {
+  //         return fs_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fs_BGK_2017_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else if (summation_small == "2014") {
+  //       if (n_terms_small == "Foster") {
+  //         return fs_eps_2014_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Navarro") {
+  //         return fs_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fs_BGK_2014_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else {
+  //       Rcerr << "error: invalid summation_small" << endl;
+  //       return NAN;
+  //     }
+  //   } else if (scale == "large") {
+  //     return fl_Nav_2009_log(rt, response, a, v, t0, w, eps);
+  //   } else if (scale == "both") {
+  //     if (summation_small == "2017") {
+  //       if (n_terms_small == "Navarro") {
+  //         return fb_Nav_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fb_BGK_Nav_2017_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else if (summation_small == "2014") {
+  //       if (n_terms_small == "Navarro") {
+  //         return fb_Nav_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else if (n_terms_small == "Kesselmeier") {
+  //         return fb_BGK_Nav_2014_log(rt, response, a, v, t0, w, sv, eps);
+  //       } else {
+  //         Rcerr << "error: invalid n_terms_small" << endl;
+  //         return NAN;
+  //       }
+  //     } else {
+  //       Rcerr << "error: invalid summation_small" << endl;
+  //       return NAN;
+  //     }
+  //   } else {
+  //     Rcerr << "error: invalid scale" << endl;
+  //     return NAN;
+  //   }
+  // }
 }
