6e5b125024181d2b6d804e44efde2b13fd00de6f
kendalfoster <kendal.foster@warwick.ac.uk>
2021-02-02 22:21:22 -0500

    updated paper_analysis ks

--- paper_analysis/section_5_benchmark/ks/ks_comparison.R
+++ paper_analysis/section_5_benchmark/ks/ks_comparison.R
@@ -54,8 +54,8 @@ ks_benchmark <- function(t, w, eps, times = 1000, unit = "ns") {
 
 ##### Run and Save the Benchmark Results #######################################
 t0 <- 1e-4 # must be nonzero for RWiener, kept for consistency
-t <- seq(0.1, 0.5, by = 0.1) - t0 # rt - t0
-w <- seq(0.3, 0.7, by = 0.2)
+t <- seq(0.1, 10, by = 0.1) - t0 # rt - t0
+w <- seq(0, 1, by = 0.1)
 eps <- 1e-6 # this is the setting from rtdists, kept for consistency
 
 ks_bm <- ks_benchmark(t = t, w = w, eps = eps, times = 1000)
@@ -65,6 +65,10 @@ save(ks_bm, compress = "xz", compression_level = 9,
 
 
 ##### Plot Benchmark Timing Results ############################################
+# uncomment the following line if loading pre-run benchmark data,
+# will load into variable named "ks_bm"
+# load(paste0(save_dir, "ks_bm.Rds"))
+
 t_idx <- match("eps", colnames(ks_bm))
 mbm_ks <- melt(ks_bm, measure.vars = -seq_len(t_idx),
                variable.name = "FuncName", value.name = "time")
@@ -72,6 +76,8 @@ mbm_ks <- melt(ks_bm, measure.vars = -seq_len(t_idx),
 Names <- c("ks_Gondan_14", "ks_Navarro_09")
 Color <- c("#d7db42", "#33cc33")
 
+
+### Violin Plot
 ggplot(mbm_ks, aes(x = factor(FuncName, levels = Names), y = time,
                     color = factor(FuncName, levels = Names),
                     fill = factor(FuncName, levels = Names))) +
@@ -97,3 +103,116 @@ ggplot(mbm_ks, aes(x = factor(FuncName, levels = Names), y = time,
              axis.title.y = element_text(size = 20),
              legend.position = "none")
 ggsave("paper_analysis/images/ks_bm.png", width = 16, height = 9)
+
+
+### Mean and Quantile Plots
+# t
+ggplot(mbm_ks, aes(x = t, y = time,
+                   color = factor(FuncName, levels = Names),
+                   fill = factor(FuncName, levels = Names))) +
+  stat_summary(fun.min = min, fun.max = max,
+               geom = "ribbon", color = NA, alpha = 0.1) +
+  stat_summary(fun.min = function(z) { quantile(z, 0.1) },
+               fun.max = function(z) { quantile(z, 0.9) },
+               geom = "ribbon", color = NA, alpha = 0.2) +
+  stat_summary(fun = mean, geom = "line") +
+  scale_color_manual(values = Color) +
+  scale_fill_manual(values = Color) +
+  labs(title = "Means of the median microbenchmark results",
+       subtitle = paste(
+         "The shaded regions represent the 10% and 90% quantiles",
+         "The lighter shaded regions represent the min and max times",
+         sep = ";\n"),
+       x = bquote(t - t[0] ~ ", normalized response time"),
+       y = "Time (microseconds)") +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 16,
+                                     margin = margin(5, 5, 15, 5, "pt")),
+        axis.text.x = element_text(size = 16, angle = 90,
+                                   vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        strip.text = element_text(size = 14),
+        strip.background = element_rect(fill = "white"),
+        legend.position = "none") +
+  facet_wrap(~ factor(FuncName, levels = Names), scales = "free_y")
+ggsave("paper_analysis/images/ks_meq_t.png", width = 16, height = 9)
+
+
+# w
+ggplot(mbm_ks, aes(x = w, y = time,
+                   color = factor(FuncName, levels = Names),
+                   fill = factor(FuncName, levels = Names))) +
+  stat_summary(fun.min = min, fun.max = max,
+               geom = "ribbon", color = NA, alpha = 0.1) +
+  stat_summary(fun.min = function(z) { quantile(z, 0.1) },
+               fun.max = function(z) { quantile(z, 0.9) },
+               geom = "ribbon", color = NA, alpha = 0.2) +
+  stat_summary(fun = mean, geom = "line") +
+  scale_color_manual(values = Color) +
+  scale_fill_manual(values = Color) +
+  labs(title = "Means of the median microbenchmark results",
+       subtitle = paste(
+         "The shaded regions represent the 10% and 90% quantiles",
+         "The lighter shaded regions represent the min and max times",
+         sep = ";\n"),
+       x = bquote(w ~ ", relative starting point (a priori bias)"),
+       y = "Time (microseconds)") +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 16,
+                                     margin = margin(5, 5, 15, 5, "pt")),
+        axis.text.x = element_text(size = 16, angle = 90,
+                                   vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        strip.text = element_text(size = 14),
+        strip.background = element_rect(fill = "white"),
+        legend.position = "none") +
+  facet_wrap(~ factor(FuncName, levels = Names), scales = "free_y")
+ggsave("paper_analysis/images/ks_meq_w.png", width = 16, height = 9)
+
+
+# eps
+ggplot(mbm_ks, aes(x = eps, y = time,
+                   color = factor(FuncName, levels = Names),
+                   fill = factor(FuncName, levels = Names))) +
+  stat_summary(fun.min = min, fun.max = max,
+               geom = "ribbon", color = NA, alpha = 0.1) +
+  stat_summary(fun.min = function(z) { quantile(z, 0.1) },
+               fun.max = function(z) { quantile(z, 0.9) },
+               geom = "ribbon", color = NA, alpha = 0.2) +
+  stat_summary(fun = mean, geom = "line") +
+  scale_x_log10() +
+  scale_color_manual(values = Color) +
+  scale_fill_manual(values = Color) +
+  labs(title = "Means of the median microbenchmark results",
+       subtitle = paste(
+         "The shaded regions represent the 10% and 90% quantiles",
+         "The lighter shaded regions represent the min and max times",
+         sep = ";\n"),
+       x = bquote("error tolerance, " ~ log[10]),
+       y = "Time (microseconds)") +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 16,
+                                     margin = margin(5, 5, 15, 5, "pt")),
+        axis.text.x = element_text(size = 16, angle = 90,
+                                   vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        strip.text = element_text(size = 14),
+        strip.background = element_rect(fill = "white"),
+        legend.position = "none") +
+  facet_wrap(~ factor(FuncName, levels = Names), scales = "free_y")
+ggsave("paper_analysis/images/ks_meq_eps.png", width = 16, height = 9)
