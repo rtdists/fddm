a6daf5912b0b99174d733e3ff3d823b4919cab65
kendalfoster <kendal.foster@warwick.ac.uk>
2021-02-15 09:06:57 -0500

    added some more ks stuff to paper_analysis

--- paper_analysis/section_5_benchmark/ks/ks_num_comp.R
+++ paper_analysis/section_5_benchmark/ks/ks_num_comp.R
@@ -7,17 +7,16 @@ save_dir <- "paper_analysis/section_5_benchmark/ks/"
 # the functions are:
   # ks_Gon(t, w, eps)
   # ks_Nav(t, w, eps) (NOTE: ks_Nav does not use the input `w`)
+	# ks_SWSE_14(t, w, eps)
+	# ks_SWSE_17(t, w, eps)
 sourceCpp(paste0(save_dir, "ks_vec.cpp"))
 
-
-
+ks_SWSE_14(0.9, 0, 1e-6)
+ks_SWSE_17(0.9, 0, 1e-6)
+ks_Gon(0.9, 0, 1e-6)
 
 
 ##### Testing Function #########################################################
-t <- seq(0.1, 10, by = 0.1)
-w <- seq(0, 1, by = 0.1)
-eps <- 10^(-1:-10)
-
 ks_num_test <- function(t, w, eps) {
   t <- t[t > 0]
   nt <- length(t)
@@ -30,7 +29,9 @@ ks_num_test <- function(t, w, eps) {
     w = rep(w, each = ne, times = nt),
     eps = rep(eps, each = 1, times = nw * nt),
     ks_Gondan_14 = ks_Gon(t, w, eps),
-    ks_Navarro_09 = ks_Nav(t, w, eps)
+    ks_Navarro_09 = ks_Nav(t, w, eps),
+		ks_SWSE_14 = ks_SWSE_14(t, w, eps),
+		ks_SWSE_17 = ks_SWSE_17(t, w, eps)
   )
 
   return(res)
@@ -39,39 +40,60 @@ ks_num_test <- function(t, w, eps) {
 
 
 ##### Run and Save the Results #################################################
+t <- seq(0.1, 10, by = 0.1)
+w <- seq(0, 1, by = 0.1)
+eps <- 10^(-1:-10)
+
 ks_num <- ks_num_test(t, w, eps)
 save(ks_num, compress = "xz", compression_level = 9,
      file = paste0(save_dir, "ks_num.Rds"))
 
+tt <- 1.3
+ww <- 0.1
+eeps <- 0.1
+gamma <- -1 / (2 * tt);
+ww * exp(gamma * ww*ww)
+sqrt(tt)/2 - ww/2
+sqrt(tt) - ww
 
+ks_num[ks_num[["ks_SWSE_17"]] - ks_num[["ks_SWSE_14"]] > 0, ]
+hist(ks_num[ks_num[["ks_SWSE_17"]] - ks_num[["ks_SWSE_14"]] > 0, "w"])
+all(ks_num[ks_num[["ks_SWSE_17"]] - ks_num[["ks_SWSE_14"]] > 0, "ks_SWSE_14"] == 1)
 
 ##### Plot Heatmap of Results ##################################################
 # uncomment the following line if loading pre-run benchmark data,
 # will load into variable named "ks_num"
 # load(paste0(save_dir, "ks_num.Rds"))
 
-ks_num[["ks_diff"]] <- ks_num[["ks_Gondan_14"]] - ks_num[["ks_Navarro_09"]]
-ks_diffs <- sort(unique(ks_num[["ks_diff"]]))
-lbl <- as.character(ks_diffs)
-lbl[ks_diffs > 0] <- paste0("+", lbl[ks_diffs > 0])
+ks_num[["ks_CGN"]] <- pmin(ks_num[["ks_Gondan_14"]], ks_num[["ks_Navarro_09"]])
+ks_num[["ks_diff_GN"]] <- ks_num[["ks_Gondan_14"]] - ks_num[["ks_Navarro_09"]]
+ks_num[["ks_diff_SC"]] <- ks_num[["ks_SWSE_17"]] - ks_num[["ks_CGN"]]
+
+
+sum(ks_num[["ks_diff_GN"]])
+sum(ks_num[["ks_diff_SC"]])
+
 w <- sort(unique(ks_num[["w"]]))
 
-sum(ks_num[["ks_diff"]])
+# Gondan - Navarro
+ks_diffs_GN <- sort(unique(ks_num[["ks_diff_GN"]]))
+lbl_GN <- as.character(ks_diffs_GN)
+lbl_GN[ks_diffs_GN > 0] <- paste0("+", lbl_GN[ks_diffs_GN > 0])
 
 for (i in 1:length(w)) {
   ggplot(ks_num[ks_num[["w"]] == w[i], ]) +
-    geom_tile(aes(x = t, y = eps, fill = ks_diff)) +
-    scale_fill_gradient2(low = "#990000",
+    geom_tile(aes(x = t, y = eps, fill = ks_diff_GN)) +
+    scale_fill_gradient2(low = "#c2a500",
                          mid = "#ffffff",
-                         high = "#000099",
+                         high = "#336600",
                          midpoint = 0,
                          name = bquote(k[s] ~ "diff"),
-                         breaks = ks_diffs,
-                         labels = lbl) +
+                         breaks = ks_diffs_GN,
+                         labels = lbl_GN) +
     scale_y_log10() +
-    labs(title = bquote("Median microbenchmark results for " ~ k[s] ~ " calculation"),
+    labs(title = bquote("Differences in " ~ k[s] ~ " calculation for Gondan (2014) - Navarro (2009)"),
          subtitle = paste0("w = ", w[i], "\n",
-                           "Cumulative difference = ", sum(ks_num[ks_num[["w"]] == w[i], "ks_diff"])),
+                           "Cumulative difference = ", sum(ks_num[ks_num[["w"]] == w[i], "ks_diff_GN"])),
          x = bquote(frac(rt, a^2) ~ ", effective response time"),
          y = bquote(epsilon ~ ", desired precision, " ~ log[10])) +
     theme_bw() +
@@ -85,6 +107,43 @@ for (i in 1:length(w)) {
           axis.title.y = element_text(size = 18),
           strip.text = element_text(size = 14),
           strip.background = element_rect(fill = "white"))
-  ggsave(paste0("paper_analysis/images/ks_heatmap/w_", i, ".png"),
+  ggsave(paste0("paper_analysis/images/ks_heatmap/Gondan_Navarro/w_", i, ".png"),
+         width = 16, height = 9)
+}
+
+
+# SWSE - min(Gondan, Navarro)
+ks_diffs_SC <- sort(unique(ks_num[["ks_diff_SC"]]))
+lbl_SC <- as.character(ks_diffs_SC)
+lbl_SC[ks_diffs_SC > 0] <- paste0("+", lbl_SC[ks_diffs_SC > 0])
+
+for (i in 1:length(w)) {
+  ggplot(ks_num[ks_num[["w"]] == w[i], ]) +
+    geom_tile(aes(x = t, y = eps, fill = ks_diff_SC)) +
+    scale_fill_gradient2(low = "#9900cc",
+                         mid = "#ffffff",
+                         high = "#336600",
+                         midpoint = 0,
+                         name = bquote(k[s] ~ "diff"),
+                         breaks = ks_diffs_SC,
+                         labels = lbl_SC) +
+    scale_y_log10() +
+    labs(title = bquote("Differences in " ~ k[s] ~ " calculation for SWSE - min(Gondan (2014), Navarro (2009) )"),
+         subtitle = paste0("w = ", w[i], "\n",
+                           "Cumulative difference = ", sum(ks_num[ks_num[["w"]] == w[i], "ks_diff_SC"])),
+         x = bquote(frac(rt, a^2) ~ ", effective response time"),
+         y = bquote(epsilon ~ ", desired precision, " ~ log[10])) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(size = 20),
+          plot.subtitle = element_text(size = 17),
+          axis.text.x = element_text(size = 16),
+          axis.text.y = element_text(size = 16),
+          axis.title.x = element_text(size = 18),
+          axis.title.y = element_text(size = 18),
+          strip.text = element_text(size = 14),
+          strip.background = element_rect(fill = "white"))
+  ggsave(paste0("paper_analysis/images/ks_heatmap/SWSE_CGN/w_", i, ".png"),
          width = 16, height = 9)
 }
--- paper_analysis/section_5_benchmark/ks/ks_vec.cpp
+++ paper_analysis/section_5_benchmark/ks/ks_vec.cpp
@@ -82,3 +82,134 @@ IntegerVector ks_Nav(const NumericVector& t, const NumericVector& w,
   }
   return out;
 }
+
+
+////////// fddm 2021 ///////////////////////////////////////////////////////////
+// [[Rcpp::export]]
+IntegerVector ks_SWSE_14(const NumericVector& t, const NumericVector& w,
+                         const NumericVector& eps)
+{ // note: t is assumed to be t := t / (a*a)
+  int nt = t.length();
+  int nw = w.length();
+  int ne = eps.length();
+  // int n = nt * nw * ne;
+  IntegerVector out(nt * nw * ne);
+
+  double gamma, sum, term, tt, ww, eeps;
+  int minterms, j;
+  int oddj = 0;
+
+  for (int ti = 0; ti < nt; ti++) {
+    tt = t[ti];
+    for (int wi = 0; wi < nw; wi++) {
+      ww = w[wi];
+      for (int ei = 0; ei < ne; ei++) {
+        eeps = eps[ei];
+
+        minterms = sqrt(tt)/2 - ww/2; // min number of terms, truncates toward 0
+        gamma = -1 / (2 * tt);
+        sum = ww * exp(gamma * ww*ww); // initialize with j=0 term
+        term = (ww > 0 || minterms > 0) ? sum: 1.1 * eeps; // bug workaround
+        j = 0;
+        while (j < minterms) {
+          j++;
+          sum -= (2*j - ww) * exp(gamma * (2*j - ww)*(2*j - ww));
+          term = (2*j + ww) * exp(gamma * (2*j + ww)*(2*j + ww));
+          sum += term;
+        }
+        while (term > eeps) {
+          j++;
+          term = (2*j - ww) * exp(gamma * (2*j - ww)*(2*j - ww));
+          sum -= term;
+          if (term <= eeps) {
+            oddj = 1;
+            break;
+          }
+          term = (2*j + ww) * exp(gamma * (2*j + ww)*(2*j + ww));
+          sum += term;
+        }
+        out[ti*nw*ne + wi*ne + ei] = 2*j + oddj;
+      }
+    }
+  }
+  return out;
+}
+
+// [[Rcpp::export]]
+IntegerVector ks_SWSE_17(const NumericVector& t, const NumericVector& w,
+                         const NumericVector& eps)
+{ // note: t is assumed to be t := t / (a*a)
+  int nt = t.length();
+  int nw = w.length();
+  int ne = eps.length();
+  // int n = nt * nw * ne;
+  IntegerVector out(nt * nw * ne);
+
+  double gamma, sum, term, rj, tt, ww, eeps;
+  int minterms, j;
+
+  for (int ti = 0; ti < nt; ti++) {
+    tt = t[ti];
+    for (int wi = 0; wi < nw; wi++) {
+      ww = w[wi];
+      for (int ei = 0; ei < ne; ei++) {
+        eeps = eps[ei];
+
+        minterms = sqrt(tt) - ww; // min number of terms, truncates toward 0
+        gamma = -1 / (2 * tt);
+        sum = ww * exp(gamma * ww*ww); // initialize with j=0 term
+        term = (ww > 0 || minterms > 0) ? sum: 1.1 * eeps; // bug workaround
+        j = 0;
+        if (minterms % 2) { // minterms is odd (and at least 1)
+          j++;
+          rj = j + 1 - ww;
+          term = rj * exp(gamma * rj*rj);
+          sum -= term;
+          while (j < minterms) { // j is currently 1
+            j++;
+            rj = j + ww;
+            sum += rj * exp(gamma * rj*rj);
+            j++;
+            rj = j + 1 - ww;
+            term = rj * exp(gamma * rj*rj);
+            sum -= term;
+          }
+          while (term > eeps) { // j is currently odd
+            j++;
+            rj = j + ww;
+            term = rj * exp(gamma * rj*rj);
+            sum += term;
+            if (term <= eeps) break;
+            j++;
+            rj = j + 1 - ww;
+            term = rj * exp(gamma * rj*rj);
+            sum -= term;
+          }
+        } else { // minterms is even (and at least 0)
+          while (j < minterms) { // j is currently 0
+            j++;
+            rj = j + 1 - ww;
+            sum -= rj * exp(gamma * rj*rj);
+            j++;
+            rj = j + ww;
+            term = rj * exp(gamma * rj*rj);
+            sum += term;
+          }
+          while (term > eeps) { // j is currently even
+            j++;
+            rj = j + 1 - ww;
+            term = rj * exp(gamma * rj*rj);
+            sum -= term;
+            if (term <= eeps) break;
+            j++;
+            rj = j + ww;
+            term = rj * exp(gamma * rj*rj);
+            sum += term;
+          }
+        }
+        out[ti*nw*ne + wi*ne + ei] = j;
+      }
+    }
+  }
+  return out;
+}
