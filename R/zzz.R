670fcf537cfaf1580f77cecd74529f7aa116c28c
kendalfoster <kendal.foster@warwick.ac.uk>
2021-02-18 14:02:13 -0500

    minor change to R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
 .onAttach <- function(libname, pkgname) {
 	if (dfddm(rt = 0.001, response = "lower",
 	          a = 5, v = -5, t0 = 1e-4, w = 0.8, sv = 1.5, sigma = 1, log = FALSE,
-						n_terms_small = "Navarro", summation_small = "2017", scale = "large",
-						max_terms_large = 1, err_tol = 1e-6) > 1e-6) {
+						n_terms_small = "Navarro", scale = "large",
+						err_tol = 1e-6) > 1e-6) {
 	  packageStartupMessage("************\n",
 	  "fddm WARNING: Current system shows numerical problems for very small effective\n",
 	  "response times using the Navarro large time method. This issue can be avoided by \n",
