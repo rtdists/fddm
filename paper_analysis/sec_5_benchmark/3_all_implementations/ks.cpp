ebd4f6d2ab167017f146be84316fc17d2bfbc5c9
kendalfoster <kendal.foster@warwick.ac.uk>
2021-03-25 19:14:50 -0400

    trimmed ks.cpp in paper_analysis

--- paper_analysis/sec_5_benchmark/3_all_implementations/ks.cpp
+++ paper_analysis/sec_5_benchmark/3_all_implementations/ks.cpp
@@ -1,8 +1,8 @@
 // C++ functions for calculating the required number of terms in the truncated
-// small-time sum. These functions are based on the ones in `src/num_funcs.cpp`,
-// but they accept a vectors as inputs and loop over them. This is done to add
-// more computation time in C++ so that the `microbenchmark` results contain
-// proportionally less overhead from R.
+// "small-time" sum. These functions are based on the ones in
+// `src/num_funcs.cpp`, but they accept a vectors as inputs and loop over them.
+// This is done to add more computation time in C++ so that the `microbenchmark`
+// results contain proportionally less overhead from R.
 
 #include <Rcpp.h>
 // [[Rcpp::plugins(cpp11)]]
@@ -19,11 +19,11 @@ using Rcpp::NumericVector;
 ////////// Gondan et al 2014 ///////////////////////////////////////////////////
 // [[Rcpp::export]]
 IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
-                     const NumericVector& eps)
+                     const NumericVector& err)
 {
   int nt = t.length();
   int nw = w.length();
-  int ne = eps.length();
+  int ne = err.length();
   int n = nt * nw * ne;
   IntegerVector out(n);
   float u_eps, arg, k1;
@@ -32,7 +32,7 @@ IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
   for (int ti = 0; ti < nt; ti++) {
     for (int wi = 0; wi < nw; wi++) {
       for (int ei = 0; ei < ne; ei++) {
-        u_eps = min(-1.0, log(2 * M_PI * t[ti]*t[ti] * eps[ei]*eps[ei])); // Safe bound for sqrt
+        u_eps = min(-1.0, log(2 * M_PI * t[ti]*t[ti] * err[ei]*err[ei])); // Safe bound for sqrt
         arg = -t[ti] * (u_eps - sqrt(-2 * u_eps - 2)); // sqrt(x) with x > 0
         k1 = (sqrt(2 * t[ti]) - w[wi])/2;
         if (k1 > INT_MAX) return INT_MAX;
@@ -57,11 +57,11 @@ IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
 ////////// Navarro and Fuss 2009 ///////////////////////////////////////////////
 // [[Rcpp::export]]
 IntegerVector ks_Nav(const NumericVector& t, const NumericVector& w,
-                     const NumericVector& eps)
+                     const NumericVector& err)
 { // note: w is not used
   int nt = t.length();
   int nw = w.length();
-  int ne = eps.length();
+  int ne = err.length();
   int n = nt * nw * ne;
   IntegerVector out(n);
   float ks, bc;
@@ -69,8 +69,8 @@ IntegerVector ks_Nav(const NumericVector& t, const NumericVector& w,
   for (int ti = 0; ti < nt; ti++) {
     for (int wi = 0; wi < nw; wi++) {
       for (int ei = 0; ei < ne; ei++) {
-        if (eps[ei] < 1 / (2 * sqrt(2 * M_PI * t[ti]))) { // if error threshold is set low enough
-          ks = 2 + sqrt(-2 * t[ti] * log(2 * eps[ei] * sqrt(2 * M_PI * t[ti])));
+        if (err[ei] < 1 / (2 * sqrt(2 * M_PI * t[ti]))) { // if error threshold is set low enough
+          ks = 2 + sqrt(-2 * t[ti] * log(2 * err[ei] * sqrt(2 * M_PI * t[ti])));
           bc = sqrt(t[ti]) + 1; // boundary conditions
           if (ks > INT_MAX || bc > INT_MAX) return INT_MAX;
           out[ti*nw*ne + wi*ne + ei] = ceil(max(ks, bc)); // ensure boundary conditions are met
--- paper_analysis/supplemental_analysis/3_all_implementations/ks.cpp
+++ paper_analysis/supplemental_analysis/3_all_implementations/ks.cpp
@@ -19,11 +19,11 @@ using Rcpp::NumericVector;
 ////////// Gondan et al 2014 ///////////////////////////////////////////////////
 // [[Rcpp::export]]
 IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
-                     const NumericVector& eps)
+                     const NumericVector& err)
 {
   int nt = t.length();
   int nw = w.length();
-  int ne = eps.length();
+  int ne = err.length();
   int n = nt * nw * ne;
   IntegerVector out(n);
   float u_eps, arg, k1;
@@ -32,7 +32,7 @@ IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
   for (int ti = 0; ti < nt; ti++) {
     for (int wi = 0; wi < nw; wi++) {
       for (int ei = 0; ei < ne; ei++) {
-        u_eps = min(-1.0, log(2 * M_PI * t[ti]*t[ti] * eps[ei]*eps[ei])); // Safe bound for sqrt
+        u_eps = min(-1.0, log(2 * M_PI * t[ti]*t[ti] * err[ei]*err[ei])); // Safe bound for sqrt
         arg = -t[ti] * (u_eps - sqrt(-2 * u_eps - 2)); // sqrt(x) with x > 0
         k1 = (sqrt(2 * t[ti]) - w[wi])/2;
         if (k1 > INT_MAX) return INT_MAX;
@@ -57,11 +57,11 @@ IntegerVector ks_Gon(const NumericVector& t, const NumericVector& w,
 ////////// Navarro and Fuss 2009 ///////////////////////////////////////////////
 // [[Rcpp::export]]
 IntegerVector ks_Nav(const NumericVector& t, const NumericVector& w,
-                     const NumericVector& eps)
+                     const NumericVector& err)
 { // note: w is not used
   int nt = t.length();
   int nw = w.length();
-  int ne = eps.length();
+  int ne = err.length();
   int n = nt * nw * ne;
   IntegerVector out(n);
   float ks, bc;
@@ -69,157 +69,14 @@ IntegerVector ks_Nav(const NumericVector& t, const NumericVector& w,
   for (int ti = 0; ti < nt; ti++) {
     for (int wi = 0; wi < nw; wi++) {
       for (int ei = 0; ei < ne; ei++) {
-        if (eps[ei] < 1 / (2 * sqrt(2 * M_PI * t[ti]))) { // if error threshold is set low enough
-          ks = 2 + sqrt(-2 * t[ti] * log(2 * eps[ei] * sqrt(2 * M_PI * t[ti])));
+        if (err[ei] < 1 / (2 * sqrt(2 * M_PI * t[ti]))) { // if error threshold is set low enough
+          ks = 2 + sqrt(-2 * t[ti] * log(2 * err[ei] * sqrt(2 * M_PI * t[ti])));
           bc = sqrt(t[ti]) + 1; // boundary conditions
           if (ks > INT_MAX || bc > INT_MAX) return INT_MAX;
           out[ti*nw*ne + wi*ne + ei] = ceil(max(ks, bc)); // ensure boundary conditions are met
         } else {
           out[ti*nw*ne + wi*ne + ei] = 2; // else return minimal kappa for that case
         }
-      }
-    }
-  }
-  return out;
-}
-
-
-////////// fddm 2021 ///////////////////////////////////////////////////////////
-// [[Rcpp::export]]
-IntegerVector ks_SWSE_17(const NumericVector& t, const NumericVector& w,
-                         const NumericVector& eps)
-{ // note: t is assumed to be t := t / (a*a)
-  int nt = t.length();
-  int nw = w.length();
-  int ne = eps.length();
-  IntegerVector out(nt * nw * ne);
-
-  double gamma, sum, term, rj, tt, ww, eeps;
-  int minterms, j;
-
-  for (int ti = 0; ti < nt; ti++) {
-    tt = t[ti];
-    for (int wi = 0; wi < nw; wi++) {
-      ww = w[wi];
-      for (int ei = 0; ei < ne; ei++) {
-        eeps = eps[ei];
-
-        minterms = sqrt(tt) - ww; // min number of terms, truncates toward 0
-        gamma = -1 / (2 * tt);
-        sum = ww * exp(gamma * ww*ww); // initialize with j=0 term
-        j = 0;
-        if (minterms % 2) { // minterms is odd (and at least 1)
-          j++;
-          rj = j + 1 - ww;
-          term = rj * exp(gamma * rj*rj);
-          sum -= term;
-          while (j < minterms) {
-            j++;
-            rj = j + ww;
-            sum += rj * exp(gamma * rj*rj);
-            j++;
-            rj = j + 1 - ww;
-            term = rj * exp(gamma * rj*rj);
-            sum -= term;
-          }
-          j++;
-          rj = j + ww; // j is now even
-          term = rj * exp(gamma * rj*rj);
-          sum += term;
-          while (term > eeps) {
-            j++;
-            rj = j + 1 - ww;
-            term = rj * exp(gamma * rj*rj);
-            sum -= term;
-            if (term <= eeps) break;
-            j++;
-            rj = j + ww;
-            term = rj * exp(gamma * rj*rj);
-            sum += term;
-          }
-        } else { // minterms is even (and at least 0)
-          while (j < minterms) { // j is currently 0
-            j++;
-            rj = j + 1 - ww;
-            sum -= rj * exp(gamma * rj*rj);
-            j++;
-            rj = j + ww;
-            term = rj * exp(gamma * rj*rj);
-            sum += term;
-          }
-          j++;
-          rj = j + 1 - ww; // j is now odd
-          term = rj * exp(gamma * rj*rj);
-          sum -= term;
-          while (term > eeps) {
-            j++;
-            rj = j + ww;
-            term = rj * exp(gamma * rj*rj);
-            sum += term;
-            if (term <= eeps) break;
-            j++;
-            rj = j + 1 - ww;
-            term = rj * exp(gamma * rj*rj);
-            sum -= term;
-          }
-        }
-        out[ti*nw*ne + wi*ne + ei] = j;
-      }
-    }
-  }
-  return out;
-}
-
-
-// [[Rcpp::export]]
-IntegerVector ks_SWSE_14(const NumericVector& t, const NumericVector& w,
-                         const NumericVector& eps)
-{ // note: t is assumed to be t := t / (a*a)
-  int nt = t.length();
-  int nw = w.length();
-  int ne = eps.length();
-  IntegerVector out(nt * nw * ne);
-
-  double gamma, sum, term, tt, ww, eeps, rj;
-  int minterms, j, oddj;
-
-  for (int ti = 0; ti < nt; ti++) {
-    tt = t[ti];
-    for (int wi = 0; wi < nw; wi++) {
-      ww = w[wi];
-      for (int ei = 0; ei < ne; ei++) {
-        eeps = eps[ei];
-
-        minterms = sqrt(tt)/2 - ww/2; // min number of terms, truncates toward 0
-        gamma = -1 / (2 * tt);
-        sum = ww * exp(gamma * ww*ww); // initialize with j=0 term
-        j = 0;
-        while (j < minterms) {
-          j++;
-          rj = 2*j - ww;
-          sum -= rj * exp(gamma * rj*rj);
-          rj = 2*j + ww;
-          sum += rj * exp(gamma * rj*rj);
-        }
-        j++;
-        rj = 2*j - ww;
-        term = rj * exp(gamma * rj*rj);
-        sum -= term;
-        oddj = 1;
-        while (term > eeps) {
-          rj = 2*j + ww;
-          term = rj * exp(gamma * rj*rj);
-          sum += term;
-          if (term <= eeps) {
-            oddj = 0;
-            break;
-          }
-          j++;
-          rj = 2*j - ww;
-          term = rj * exp(gamma * rj*rj);
-          sum -= term;
-        }
-        out[ti*nw*ne + wi*ne + ei] = 2*j - oddj;
       }
     }
   }
