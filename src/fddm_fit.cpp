c93a2cd8dc8ce76bf90df618df143906f624dc64
kendalfoster <kendal.foster@warwick.ac.uk>
2022-10-19 11:13:25 -0400

    uncommented functions

--- src/fddm_fit.cpp
+++ src/fddm_fit.cpp
@@ -1,112 +1,112 @@
-// // Log-likelihood and gradient function for the Ratcliff DDM
-// #include "helper_functions/fitting/class_methods.h"
+// Log-likelihood and gradient function for the Ratcliff DDM
+#include "helper_functions/fitting/class_methods.h"
 
 
 
-// //' @export fddm_fit
-// class fddm_fit {
-//   public:
-//     // variables (accessible from R)
-//     vector<double> rt {};
-//     vector<double> response {};
-//     MatrixXd mm_v {};
-//     MatrixXd mm_a {};
-//     MatrixXd mm_t0 {};
-//     MatrixXd mm_w {};
-//     MatrixXd mm_sv {};
-//     double err_tol {0.000001};
-//     double switch_thresh {0.8};
-//     vector<double> likelihood {};
-//     VectorXd coefs {};
-//     MatrixXd hess_v {};
-//     MatrixXd hess_a {};
-//     MatrixXd hess_t0 {};
-//     MatrixXd hess_w {};
-//     MatrixXd hess_sv {};
-//     MatrixXd vcov_v {};
-//     MatrixXd vcov_a {};
-//     MatrixXd vcov_t0 {};
-//     MatrixXd vcov_w {};
-//     MatrixXd vcov_sv {};
+//' @export fddm_fit
+class fddm_fit {
+  public:
+    // variables (accessible from R)
+    vector<double> rt {};
+    vector<double> response {};
+    MatrixXd mm_v {};
+    MatrixXd mm_a {};
+    MatrixXd mm_t0 {};
+    MatrixXd mm_w {};
+    MatrixXd mm_sv {};
+    double err_tol {0.000001};
+    double switch_thresh {0.8};
+    vector<double> likelihood {};
+    VectorXd coefs {};
+    MatrixXd hess_v {};
+    MatrixXd hess_a {};
+    MatrixXd hess_t0 {};
+    MatrixXd hess_w {};
+    MatrixXd hess_sv {};
+    MatrixXd vcov_v {};
+    MatrixXd vcov_a {};
+    MatrixXd vcov_t0 {};
+    MatrixXd vcov_w {};
+    MatrixXd vcov_sv {};
 
-//     // variables (internal use only)
-//     int Nrt {};
-//     double rt0 {1e6};
-//     vector<int> form_len {0, 0, 0, 0, 0}; // length of each parameter's formula
-//     int Ncoefs {0};
-//     VectorXd v {};
-//     VectorXd a {};
-//     VectorXd t0 {};
-//     VectorXd w {};
-//     VectorXd sv {};
+    // variables (internal use only)
+    int Nrt {};
+    double rt0 {1e6};
+    vector<int> form_len {0, 0, 0, 0, 0}; // length of each parameter's formula
+    int Ncoefs {0};
+    VectorXd v {};
+    VectorXd a {};
+    VectorXd t0 {};
+    VectorXd w {};
+    VectorXd sv {};
 
-//     // constructors
-//     fddm_fit(const vector<double>& rt_vector,
-//              const SEXP& response_vector,
-//              const vector<MatrixXd>& model_matrices,
-//              const double& error_tolerance,
-//              const double& switching_threshold);
+    // constructors
+    fddm_fit(const vector<double>& rt_vector,
+             const SEXP& response_vector,
+             const vector<MatrixXd>& model_matrices,
+             const double& error_tolerance,
+             const double& switching_threshold);
 
-//     // methods
-//     double calc_loglik(const VectorXd& temp_params);
-//     VectorXd calc_gradient(const VectorXd& temp_params);
-//     void calc_hessians(const VectorXd& temp_params);
-//     void calc_vcov();
-//     VectorXd calc_std_err();
-// };
-
-
-
-// RCPP_MODULE(fddm_fit) {
-//   using namespace Rcpp;
-
-//   class_<fddm_fit>( "fddm_fit")
-//     .constructor<vector<double>, SEXP, vector<MatrixXd>, double, double>("Constructor given response times, responses, model matrices, error tolerance, and switching threshold")
-//     .field("rt", &fddm_fit::rt)
-//     .field("response", &fddm_fit::response)
-//     .field("modmat_v", &fddm_fit::mm_v)
-//     .field("modmat_a", &fddm_fit::mm_a)
-//     .field("modmat_t0", &fddm_fit::mm_t0)
-//     .field("modmat_w", &fddm_fit::mm_w)
-//     .field("modmat_sv", &fddm_fit::mm_sv)
-//     .field("err_tol", &fddm_fit::err_tol)
-//     .field("switch_thresh", &fddm_fit::switch_thresh)
-//     .field("likelihood", &fddm_fit::likelihood)
-//     .field("coefficients", &fddm_fit::coefs)
-//     .field("hess_v", &fddm_fit::hess_v)
-//     .field("hess_a", &fddm_fit::hess_a)
-//     .field("hess_t0", &fddm_fit::hess_t0)
-//     .field("hess_w", &fddm_fit::hess_w)
-//     .field("hess_sv", &fddm_fit::hess_sv)
-//     .field("vcov_v", &fddm_fit::vcov_v)
-//     .field("vcov_a", &fddm_fit::vcov_a)
-//     .field("vcov_t0", &fddm_fit::vcov_t0)
-//     .field("vcov_w", &fddm_fit::vcov_w)
-//     .field("vcov_sv", &fddm_fit::vcov_sv)
-//     .method("calculate_loglik", &fddm_fit::calc_loglik)
-//     .method("calculate_gradient", &fddm_fit::calc_gradient)
-//     .method("calculate_hessians", &fddm_fit::calc_hessians)
-//     .method("calculate_vcov", &fddm_fit::calc_vcov)
-//     .method("calculate_standard_error", &fddm_fit::calc_std_err)
-//   ;
-// }
+    // methods
+    double calc_loglik(const VectorXd& temp_params);
+    VectorXd calc_gradient(const VectorXd& temp_params);
+    void calc_hessians(const VectorXd& temp_params);
+    void calc_vcov();
+    VectorXd calc_std_err();
+};
 
 
 
-// // This class requires inputs of:
-// //
-// // 1. rt (response times)
-// // 2. response (associated responses)
-// // 3. list of model matrices (in order: v, a, t0, w, sv)
-// // 4. [optional] error_tolerance (for likelihood calculation)
-// // 5. [optional] switching threshold (for likelihood calculation)
-// //
-// //
-// // Note on model matrices:
-// // The model matrix can be a 1x1 matrix with a single value. In this case, the
-// // respective DDM parameter is treated as a constant, and the single value in
-// // the 1x1 model matrix will be used as a constant value during the fitting.
-// // The model matrices can also be an nx1 column vector, indicating a formula
-// // with a single feature (i.e., ~ 1 or ~ 0 + feat). Note that if the data has
-// // only one observation, this will result in the model matrix being read as a
-// // constant.
+RCPP_MODULE(fddm_fit) {
+  using namespace Rcpp;
+
+  class_<fddm_fit>( "fddm_fit")
+    .constructor<vector<double>, SEXP, vector<MatrixXd>, double, double>("Constructor given response times, responses, model matrices, error tolerance, and switching threshold")
+    .field("rt", &fddm_fit::rt)
+    .field("response", &fddm_fit::response)
+    .field("modmat_v", &fddm_fit::mm_v)
+    .field("modmat_a", &fddm_fit::mm_a)
+    .field("modmat_t0", &fddm_fit::mm_t0)
+    .field("modmat_w", &fddm_fit::mm_w)
+    .field("modmat_sv", &fddm_fit::mm_sv)
+    .field("err_tol", &fddm_fit::err_tol)
+    .field("switch_thresh", &fddm_fit::switch_thresh)
+    .field("likelihood", &fddm_fit::likelihood)
+    .field("coefficients", &fddm_fit::coefs)
+    .field("hess_v", &fddm_fit::hess_v)
+    .field("hess_a", &fddm_fit::hess_a)
+    .field("hess_t0", &fddm_fit::hess_t0)
+    .field("hess_w", &fddm_fit::hess_w)
+    .field("hess_sv", &fddm_fit::hess_sv)
+    .field("vcov_v", &fddm_fit::vcov_v)
+    .field("vcov_a", &fddm_fit::vcov_a)
+    .field("vcov_t0", &fddm_fit::vcov_t0)
+    .field("vcov_w", &fddm_fit::vcov_w)
+    .field("vcov_sv", &fddm_fit::vcov_sv)
+    .method("calculate_loglik", &fddm_fit::calc_loglik)
+    .method("calculate_gradient", &fddm_fit::calc_gradient)
+    .method("calculate_hessians", &fddm_fit::calc_hessians)
+    .method("calculate_vcov", &fddm_fit::calc_vcov)
+    .method("calculate_standard_error", &fddm_fit::calc_std_err)
+  ;
+}
+
+
+
+// This class requires inputs of:
+//
+// 1. rt (response times)
+// 2. response (associated responses)
+// 3. list of model matrices (in order: v, a, t0, w, sv)
+// 4. [optional] error_tolerance (for likelihood calculation)
+// 5. [optional] switching threshold (for likelihood calculation)
+//
+//
+// Note on model matrices:
+// The model matrix can be a 1x1 matrix with a single value. In this case, the
+// respective DDM parameter is treated as a constant, and the single value in
+// the 1x1 model matrix will be used as a constant value during the fitting.
+// The model matrices can also be an nx1 column vector, indicating a formula
+// with a single feature (i.e., ~ 1 or ~ 0 + feat). Note that if the data has
+// only one observation, this will result in the model matrix being read as a
+// constant.
--- src/pfddm.cpp
+++ src/pfddm.cpp
@@ -1,189 +1,189 @@
-// // Distribution function for the Ratcliff Diffusion Decision Model (DDM)
+// Distribution function for the Ratcliff Diffusion Decision Model (DDM)
 
-// #include "helper_functions/distribution/declarations.h"
-// #include "helper_functions/distribution/helper_funcs.h"
-// #include "helper_functions/distribution/distribution_funcs.h"
+#include "helper_functions/distribution/declarations.h"
+#include "helper_functions/distribution/helper_funcs.h"
+#include "helper_functions/distribution/distribution_funcs.h"
 
 
 
-// //' Distribution of Ratcliff Diffusion Decision Model
-// //'
-// //' Density function for the Ratcliff diffusion decision model (DDM) with
-// //' following parameters: \code{v} (drift rate), \code{a} (threshold
-// //' separation), \code{t0} (non-decision time/response time constant), \code{w}
-// //' (relative starting point), \code{sv} (inter-trial variability of drift), and
-// //' \code{sigma} (diffusion coefficient of underlying Wiener process).
-// //'
-// //'
-// //'
-// //' @param rt A vector of response times (in seconds). If a response time is
-// //'   non-positve, then its density will evaluate to \eqn{0} if log = FALSE and
-// //'   \ifelse{html}{\out{-&#8734}}{\eqn{-\infty}} if log = TRUE.
-// //'
-// //' @param response Binary response(s) that correspond(s) to either the "lower"
-// //'   or "upper" threshold. This model parameter can either be a singular value
-// //'   or a vector. The value(s) in 'response' can be of the following data
-// //'   types:
-// //'     \itemize{
-// //'       \item integers or doubles (\eqn{1}
-// //'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower", \eqn{2}
-// //'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper");
-// //'       \item factors (the first level gets mapped to "lower", and the second
-// //'         level gets mapped to "upper"; any additional levels are ignored).
-// //'       \item strings (only the first character is checked, "L"
-// //'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower" or "U"
-// //'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper", case insensitive);
-// //'       \item logicals (FALSE \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower",
-// //'         TRUE \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper");
-// //'     }
-// //'
-// //' @param v Drift rate. Average slope of the information accumulation process.
-// //'   The drift gives information about the speed and direction of the
-// //'   accumulation of information. Large (absolute) values of drift indicate a
-// //'   good performance. If received information supports the response linked to
-// //'   the upper threshold, then the sign will be positive; similarly a negative
-// //'   value indicates that the received information supports the response linked
-// //'   to the lower threshold. Allowed range: \code{v} is a real number. Typical
-// //'   range: \eqn{-5 <} \code{v} \eqn{< 5}.
-// //'
-// //' @param a Threshold separation. Amount of information that is considered for
-// //'   a decision. Large values indicate a conservative decisional style. Allowed
-// //'   range: \eqn{0 <} \code{a}. Typical range: \eqn{0.5 <} \code{a} \eqn{< 2}.
-// //'
-// //' @param t0 Non-decision time or response time constant (in seconds). Lower
-// //'   bound for the duration of all non-decisional processes (encoding and
-// //'   response execution). If this value is greater than \code{rt}, then the
-// //'   resulting density is returned as if \code{rt} \eqn{ \le 0}. Allowed range:
-// //'   \eqn{0 \le} \code{t0}. Typical range: \eqn{0.1 <} \code{t0} \eqn{< 0.5}.
-// //'
-// //' @param w Relative starting point. Indicator of an a priori bias in decision
-// //'   making. When the relative starting point \code{w} deviates from \eqn{0.5},
-// //'   the amount of information necessary for a decision differs between
-// //'   response alternatives. Allowed range: \eqn{0 <} \code{w} \eqn{< 1}.
-// //'   Default value is \eqn{0.5} (i.e., no bias).
-// //'
-// //' @param sv Inter-trial-variability of drift rate. Standard deviation of a
-// //'   normal distribution with mean \code{v} describing the distribution of
-// //'   actual drift rates from specific trials. Values different from \eqn{0} can
-// //'   predict slow errors. Allowed range: \eqn{0 \le} \code{sv}. Typical range:
-// //'   \eqn{0 <} \code{sv} \eqn{< 2}. Default value is \eqn{0}, which indicates
-// //'   no drift in the function call. See Details for more information.
-// //'
-// //' @param sigma Diffusion coefficient of the underlying Wiener process. Allowed
-// //'   range: \eqn{0 <} \code{sigma}. Default value is \eqn{1}. This parameter
-// //'   simply scales the parameters \code{a}, \code{v}, and \code{sv} as follows.
-// //'   See Details for more information. \itemize{ \item \code{a}
-// //'   \ifelse{html}{\out{&#8594;}}{\eqn{\to}} \code{a} \eqn{/} \code{sigma}
-// //'   \item \code{v} \ifelse{html}{\out{&#8594;}}{\eqn{\to}} \code{v} \eqn{/}
-// //'   \code{sigma} \item \code{sv} \ifelse{html}{\out{&#8594;}}{\eqn{\to}}
-// //'   \code{sv} \eqn{/} \code{sigma} }.
-// //'
-// //' @param log Logical; if \code{TRUE}, probabilities \eqn{p} are given as
-// //'   \eqn{log(p)}. Default is \code{FALSE}.
-// //' 
-// //' @param method Which method of calculating the CDF to use. Can be one of
-// //'   \{\code{"Mills"}, \code{"NCDF"}\}, case-insensitive. Default is
-// //'   \code{"Mills"}.
-// //'
-// //' @param err_tol Allowed error tolerance of the density function. Since the
-// //'   density function contains an infinite sum, this parameter defines the
-// //'   precision of the approximation to that infinite sum. If the provided
-// //'   error tolerance is less than \eqn{1e-300}, it is set to \eqn{1e-300}.
-// //'   Default is \eqn{1e-6}.
-// //'
-// //'
-// //'
-// //' @details
-// //'
-// //' All of the model inputs and parameters (\code{rt}, \code{response},
-// //' \code{v}, \code{a}, \code{t0}, \code{w}, \code{sv}, \code{sigma}) can be
-// //' input as a single value or as a vector of values. If input as a vector of
-// //' values, then the standard \code{R} input wrapping will occur.
-// //'
-// //' \code{sigma} - The default value of this parameter is \code{1} because it
-// //' only scales the parameters \code{v}, \code{a}, and \code{sv}, as shown
-// //' above. However, other formulations of the DDM may set \code{sigma = 0.1}
-// //' (see Ratcliff (1978), the fourth reference), so care must be taken when
-// //' comparing the results of different formulations.
-// //'
-// //'
-// //'
-// //' @references Navarro, D. J., & Fuss, I. G. (2009). Fast and accurate
-// //'   calculations for first-passage times in Wiener diffusion models. Journal
-// //'   of Mathematical Psychology, 53(4), 222-230.
-// //'
-// //'   Gondan, M., Blurton, S. P., & Kesselmeier, M. (2014). Even faster and even
-// //'   more accurate first-passage time densities and distributions for the
-// //'   Wiener diffusion model. Journal of Mathematical Psychology, 60, 20-22.
-// //'
-// //'   Blurton, S. P., Kesselmeier, M., & Gondan, M. (2017). The first-passage
-// //'   time distribution for the diffusion model with variable drift. Journal of
-// //'   Mathematical Psychology, 76, 7-12.
-// //'
-// //'   Ratcliff, R. (1978). A theory of memory retrieval. Psychological review,
-// //'   85(2), 59.
-// //'
-// //'
-// //'
-// //' @example examples/examples.distribution.R
-// //'
-// //'
-// //'
-// //' @return A vector containing the distribution of the DDM with precision
-// //'   \code{err_tol} whose length matches that of the longest input parameter
-// //'   (usually \code{rt}).
-// //'
-// //' @useDynLib fddm, .registration = TRUE
-// //' @import Rcpp
-// //' @export
-// // [[Rcpp::export]]
-// NumericVector pfddm(const NumericVector& rt,
-//                     const SEXP& response,
-//                     const NumericVector& v,
-//                     const NumericVector& a,
-//                     const NumericVector& t0,
-//                     const NumericVector& w = 0.5,
-//                     const NumericVector& sv = 0.0,
-//                     const NumericVector& sigma = 1.0,
-//                     NumericVector err_tol = 0.000001,
-//                     const bool& log = false,
-//                     const std::string& method = "Mills")
-// {
-//   // determine which method to use (also log or non-log)
-//   DisFunc disf;
-//   double rt0;
+//' Distribution of Ratcliff Diffusion Decision Model
+//'
+//' Density function for the Ratcliff diffusion decision model (DDM) with
+//' following parameters: \code{v} (drift rate), \code{a} (threshold
+//' separation), \code{t0} (non-decision time/response time constant), \code{w}
+//' (relative starting point), \code{sv} (inter-trial variability of drift), and
+//' \code{sigma} (diffusion coefficient of underlying Wiener process).
+//'
+//'
+//'
+//' @param rt A vector of response times (in seconds). If a response time is
+//'   non-positve, then its density will evaluate to \eqn{0} if log = FALSE and
+//'   \ifelse{html}{\out{-&#8734}}{\eqn{-\infty}} if log = TRUE.
+//'
+//' @param response Binary response(s) that correspond(s) to either the "lower"
+//'   or "upper" threshold. This model parameter can either be a singular value
+//'   or a vector. The value(s) in 'response' can be of the following data
+//'   types:
+//'     \itemize{
+//'       \item integers or doubles (\eqn{1}
+//'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower", \eqn{2}
+//'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper");
+//'       \item factors (the first level gets mapped to "lower", and the second
+//'         level gets mapped to "upper"; any additional levels are ignored).
+//'       \item strings (only the first character is checked, "L"
+//'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower" or "U"
+//'         \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper", case insensitive);
+//'       \item logicals (FALSE \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "lower",
+//'         TRUE \ifelse{html}{\out{&#8594;}}{\eqn{\to}} "upper");
+//'     }
+//'
+//' @param v Drift rate. Average slope of the information accumulation process.
+//'   The drift gives information about the speed and direction of the
+//'   accumulation of information. Large (absolute) values of drift indicate a
+//'   good performance. If received information supports the response linked to
+//'   the upper threshold, then the sign will be positive; similarly a negative
+//'   value indicates that the received information supports the response linked
+//'   to the lower threshold. Allowed range: \code{v} is a real number. Typical
+//'   range: \eqn{-5 <} \code{v} \eqn{< 5}.
+//'
+//' @param a Threshold separation. Amount of information that is considered for
+//'   a decision. Large values indicate a conservative decisional style. Allowed
+//'   range: \eqn{0 <} \code{a}. Typical range: \eqn{0.5 <} \code{a} \eqn{< 2}.
+//'
+//' @param t0 Non-decision time or response time constant (in seconds). Lower
+//'   bound for the duration of all non-decisional processes (encoding and
+//'   response execution). If this value is greater than \code{rt}, then the
+//'   resulting density is returned as if \code{rt} \eqn{ \le 0}. Allowed range:
+//'   \eqn{0 \le} \code{t0}. Typical range: \eqn{0.1 <} \code{t0} \eqn{< 0.5}.
+//'
+//' @param w Relative starting point. Indicator of an a priori bias in decision
+//'   making. When the relative starting point \code{w} deviates from \eqn{0.5},
+//'   the amount of information necessary for a decision differs between
+//'   response alternatives. Allowed range: \eqn{0 <} \code{w} \eqn{< 1}.
+//'   Default value is \eqn{0.5} (i.e., no bias).
+//'
+//' @param sv Inter-trial-variability of drift rate. Standard deviation of a
+//'   normal distribution with mean \code{v} describing the distribution of
+//'   actual drift rates from specific trials. Values different from \eqn{0} can
+//'   predict slow errors. Allowed range: \eqn{0 \le} \code{sv}. Typical range:
+//'   \eqn{0 <} \code{sv} \eqn{< 2}. Default value is \eqn{0}, which indicates
+//'   no drift in the function call. See Details for more information.
+//'
+//' @param sigma Diffusion coefficient of the underlying Wiener process. Allowed
+//'   range: \eqn{0 <} \code{sigma}. Default value is \eqn{1}. This parameter
+//'   simply scales the parameters \code{a}, \code{v}, and \code{sv} as follows.
+//'   See Details for more information. \itemize{ \item \code{a}
+//'   \ifelse{html}{\out{&#8594;}}{\eqn{\to}} \code{a} \eqn{/} \code{sigma}
+//'   \item \code{v} \ifelse{html}{\out{&#8594;}}{\eqn{\to}} \code{v} \eqn{/}
+//'   \code{sigma} \item \code{sv} \ifelse{html}{\out{&#8594;}}{\eqn{\to}}
+//'   \code{sv} \eqn{/} \code{sigma} }.
+//'
+//' @param log Logical; if \code{TRUE}, probabilities \eqn{p} are given as
+//'   \eqn{log(p)}. Default is \code{FALSE}.
+//' 
+//' @param method Which method of calculating the CDF to use. Can be one of
+//'   \{\code{"Mills"}, \code{"NCDF"}\}, case-insensitive. Default is
+//'   \code{"Mills"}.
+//'
+//' @param err_tol Allowed error tolerance of the density function. Since the
+//'   density function contains an infinite sum, this parameter defines the
+//'   precision of the approximation to that infinite sum. If the provided
+//'   error tolerance is less than \eqn{1e-300}, it is set to \eqn{1e-300}.
+//'   Default is \eqn{1e-6}.
+//'
+//'
+//'
+//' @details
+//'
+//' All of the model inputs and parameters (\code{rt}, \code{response},
+//' \code{v}, \code{a}, \code{t0}, \code{w}, \code{sv}, \code{sigma}) can be
+//' input as a single value or as a vector of values. If input as a vector of
+//' values, then the standard \code{R} input wrapping will occur.
+//'
+//' \code{sigma} - The default value of this parameter is \code{1} because it
+//' only scales the parameters \code{v}, \code{a}, and \code{sv}, as shown
+//' above. However, other formulations of the DDM may set \code{sigma = 0.1}
+//' (see Ratcliff (1978), the fourth reference), so care must be taken when
+//' comparing the results of different formulations.
+//'
+//'
+//'
+//' @references Navarro, D. J., & Fuss, I. G. (2009). Fast and accurate
+//'   calculations for first-passage times in Wiener diffusion models. Journal
+//'   of Mathematical Psychology, 53(4), 222-230.
+//'
+//'   Gondan, M., Blurton, S. P., & Kesselmeier, M. (2014). Even faster and even
+//'   more accurate first-passage time densities and distributions for the
+//'   Wiener diffusion model. Journal of Mathematical Psychology, 60, 20-22.
+//'
+//'   Blurton, S. P., Kesselmeier, M., & Gondan, M. (2017). The first-passage
+//'   time distribution for the diffusion model with variable drift. Journal of
+//'   Mathematical Psychology, 76, 7-12.
+//'
+//'   Ratcliff, R. (1978). A theory of memory retrieval. Psychological review,
+//'   85(2), 59.
+//'
+//'
+//'
+//' @example examples/examples.distribution.R
+//'
+//'
+//'
+//' @return A vector containing the distribution of the DDM with precision
+//'   \code{err_tol} whose length matches that of the longest input parameter
+//'   (usually \code{rt}).
+//'
+//' @useDynLib fddm, .registration = TRUE
+//' @import Rcpp
+//' @export
+// [[Rcpp::export]]
+NumericVector pfddm(const NumericVector& rt,
+                    const SEXP& response,
+                    const NumericVector& v,
+                    const NumericVector& a,
+                    const NumericVector& t0,
+                    const NumericVector& w = 0.5,
+                    const NumericVector& sv = 0.0,
+                    const NumericVector& sigma = 1.0,
+                    NumericVector err_tol = 0.000001,
+                    const bool& log = false,
+                    const std::string& method = "Mills")
+{
+  // determine which method to use (also log or non-log)
+  DisFunc disf;
+  double rt0;
   
-//   determine_method(method, disf, rt0, log);
+  determine_method(method, disf, rt0, log);
   
-//   // determine lengths of parameter inputs, except response
-//   int Nrt  = rt.length();
-//   int Nv   = v.length();
-//   int Na   = a.length();
-//   int Nt0  = t0.length();
-//   int Nw   = w.length();
-//   int Nsv  = sv.length();
-//   int Nsig = sigma.length();
-//   int Nerr = err_tol.length();
-//   int Nmax = max({Nrt, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr}); // include Nres later
-//   int Nres;
+  // determine lengths of parameter inputs, except response
+  int Nrt  = rt.length();
+  int Nv   = v.length();
+  int Na   = a.length();
+  int Nt0  = t0.length();
+  int Nw   = w.length();
+  int Nsv  = sv.length();
+  int Nsig = sigma.length();
+  int Nerr = err_tol.length();
+  int Nmax = max({Nrt, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr}); // include Nres later
+  int Nres;
 
 
-//   // initialize output, resized in convert_responses() inside parameter_check()
-//   vector<double> out;
+  // initialize output, resized in convert_responses() inside parameter_check()
+  vector<double> out;
 
 
-//   // check for invalid inputs, invalid inputs get marked in the vector `out`
-//   if (!parameter_check(Nrt, Nres, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr, Nmax,
-//                        rt, response, v, a, t0, w, sv, sigma, err_tol,
-//                        out, rt0)) {
-//     NumericVector empty_out(0);
-//     return empty_out;
-//   }
+  // check for invalid inputs, invalid inputs get marked in the vector `out`
+  if (!parameter_check(Nrt, Nres, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr, Nmax,
+                       rt, response, v, a, t0, w, sv, sigma, err_tol,
+                       out, rt0)) {
+    NumericVector empty_out(0);
+    return empty_out;
+  }
 
-//   // loop through all inputs, the vector `out` gets updated
-//   calculate_cdf(Nrt, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr,
-//                 Nmax, rt, v, a, t0, w, sv, sigma,
-//                 err_tol, out, rt0, disf);
+  // loop through all inputs, the vector `out` gets updated
+  calculate_cdf(Nrt, Nv, Na, Nt0, Nw, Nsv, Nsig, Nerr,
+                Nmax, rt, v, a, t0, w, sv, sigma,
+                err_tol, out, rt0, disf);
 
-//   return Rcpp::wrap(out);
-// }
+  return Rcpp::wrap(out);
+}
